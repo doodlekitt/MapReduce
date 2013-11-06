cbe8799018dc8e07427838c9d70c75a35e9c4b99
Shannon Williams <srw@andrew.cmu.edu>
2013-11-05 21:08:00 -0500

    Adding DistFileSystem

--- DistFileSystem.java
+++ DistFileSystem.java
@@ -1,6 +1,12 @@
 public class DistFileSystem {
 
+    private HashMap<Integer, List<String>> files =  new HashMap<Integer, List<String>>();
+
     int recordsperfile = 100;
+
+    public ListFileLocations(String filename) {
+
+    }
 
     public SplitFile(String filepath, int recordsize) throws FileNotFoundException {
         File file = new File(filepath);
