e3936676b705c47075d4e8fd8991b19c1db511cb
Rhiannon Malia <rmalia@andrew.cmu.edu>
2013-11-13 21:19:31 -0500

    Fixed wordcount to match added to makefile

--- WordCount.java
+++ WordCount.java
@@ -2,54 +2,21 @@ import java.io.*;
 import java.lang.*;
 import java.util.*;
 
-public static class Tuple{
-    public String key;
-    public int count;
+public class WordCount implements MapClass{
 
-    public Tuple(String key, int count){
-	this.key = key;
-	this.count = count;
+    public Map.Entry<String, Integer> map(String input){
+	Map.Entry<String, Integer> entry = 
+	    new AbstractMap.SimpleEntry<String, Integer>(input, 1);
+    	return entry;
     }
-}
 
-public class WordCount extends MapClass{
-
-    public Object map(String input){
-	String tuple = input + " 1";
-	return ans;	
-    }
-    public Object combine(String[] input){
-	Hashtable<String, int> comb = new Hashtable<String, int>();
-        BufferedReader in = null;
-        String kv = null;
-        for(int i = 0; i < input.length; i++){
-            in = new BufferedReader(new FileInputStream(input[i]);
-            while(true){
-                kv = in.readLine();
-                if(kv == null) break;
-                else{
-		    String[] tuple = kv.split();
-		    String key = tuple[0];
-		    int count = Integer.valueOf(tuple[1]).intValue();
-		    if(comb.get() != null){
-			int temp = comb.get(key) + count;
-			comb.put(key, temp);			
-		    }else{
-			comb.put(key, count);
-		    }
-                }
-            }
-            in.close();
-        }
-        return comb;
-    }
-    public Object reduce(Object input){
-	String wordcount = null;
-	Hashtable<String, int> in = (Hashtable<String, int>) input;
-	for(String word : in.keySet()){
-	    wordcount += word + ": " + in.get(word)+"\n";
-	} 
-	return wordcount;
+    public Map.Entry<?, ?> reduce(Map.Entry<?, List<?>> input){
+	Integer sum = 0;
+	for(Object i: input.getValue()) {
+	    sum +=((Integer) i).intValue();
+	}
+	String k = (String)input.getKey();
+	return new AbstractMap.SimpleEntry<String, Integer>(k, sum);
     }
 
 }
--- makefile
+++ makefile
@@ -13,6 +13,7 @@ CLASSES = \
         Master.java \
         Node.java \
         AddMap.java \
+	WordCount.java \
 
 default: classes
 
